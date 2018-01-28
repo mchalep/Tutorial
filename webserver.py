efe65131ad1be086ab5735f73221d6eb7dc25c2a
mchalep <pmchale@squiz.co.nz>
2018-01-29 11:06:52 +1300

    Updated

--- webserver.py
+++ webserver.py
@@ -1,10 +1,11 @@
-def add(num1,num2):
-    """Returms num1 + num2"""
-    return num1 + num2
+from flask import Flask
 
+app=Flask(__name__)
 
-def main ():
-    print(add(1,1))
-    print(add(2,6))
+@app.route('/')
 
-main()
+def hello():
+    return "hello, my name is Patrick"
+
+if __name__ =="__main__":
+    app.run()
